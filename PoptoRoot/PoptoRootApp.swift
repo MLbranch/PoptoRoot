6140cfe7466bb71ff819c43bfef5003ca1c3747f
Qazi Ammar Arshad <qaziammar.g@gmail.com>
2022-06-09 13:06:22 +0500

    Initial Commit

---
+++ PoptoRoot/PoptoRootApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoptoRootApp.swift
+//  PoptoRoot
+//
+//  Created by Qazi Ammar Arshad on 09/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PoptoRootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
